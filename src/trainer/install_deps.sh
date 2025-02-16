845697dbf170f3ddcbac278f9ed1d78e88c38f36
Steve <tranducnhuan1994@gmail.com>
2025-02-16 12:00:47 +0700

    Update trainer script

--- src/trainer/install_deps.sh
+++ src/trainer/install_deps.sh
@@ -2,5 +2,5 @@ sudo apt update
 sudo apt install -y python3-pip
 pip3 install -U pip uv
 export PATH=$PATH:/home/ubuntu/.local/bin
-uv pip install --system -U accelerate datasets evaluate unsloth "numpy<2" qwen-vl-utils deepspeed==0.15.4 trl==0.14.0 liger_kernel transformers rouge_score
+pip3 install -U accelerate datasets evaluate unsloth "numpy<2" qwen-vl-utils deepspeed==0.15.4 trl==0.14.0 liger_kernel transformers rouge_score tensorboardX
 mkdir training
