03ab3904b3b870125c8112127aba9c6a3d888603
Steve <tranducnhuan1994@gmail.com>
2025-02-17 14:05:38 +0700

    Update install_deps

--- src/trainer/install_deps.sh
+++ src/trainer/install_deps.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install -y python3-pip
+sudo apt install -y python3-pip tmux htop
 pip3 install -U pip uv
 export PATH=$PATH:/home/ubuntu/.local/bin
 pip3 install -U accelerate datasets evaluate unsloth "numpy<2" qwen-vl-utils deepspeed==0.15.4 trl==0.14.0 liger_kernel transformers rouge_score tensorboardX
